60db5fa2906b14342edd20a78c0710985f0cd2de
spoolphiz <alexander.manea@gmail.com>
2014-12-14 16:35:52 -0800

    initial

---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require('express'),
+	config = require('config'),
+	app = express(),
+	mailer = require('nodemailer');
+
+console.log(config.from);
+
+var transporter = mailer.createTransport({
+  service: 'Gmail',
+  auth: {
+      user: config.from.email,
+      pass: config.from.pw
+  },
+  secure: true
+});
+
+var mailOptions = {
+    from: config.from.email,
+    // to: config.triggerTarget,
+    to: 'alexander.manea@gmail.com'
+};
+
+app.get('/build/success', function(req, res, next) {
+	mailOptions.subject = config.buildTags.success;
+
+	transporter.sendMail(mailOptions, function(err, info){
+		if(err){
+			var resData = {message: err.message};
+			res.status(500).json(resData);
+			return console.log(err);
+		}
+		
+		console.log('Message sent: ' + info.response);
+		var resData = {message: info.response};
+		res.status(200).json(resData);
+	});
+
+	
+});
+
+var server = app.listen(4040, function() {
+	var host = server.address().address
+	var port = server.address().port
+
+	console.log('Blinky listening at http://%s:%s', host, port)
+});
